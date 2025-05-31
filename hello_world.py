fbe4c9c9250ab973897971e85dd1ae98d5dde5d5
efricklab <erfrick@gmail.com>
2025-05-31 16:02:05 -0400

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+# hello_world.py
+
+# This is a simple Python program that prints "Hello, World!" to the screen.
+
+# The print() function displays the specified message to the screen.
+print("Hello, World!")
